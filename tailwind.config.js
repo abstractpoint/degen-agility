0223025d445c52ea468323eddc90241cbe15f69b
George <george@abstractpoint.co.uk>
2022-03-27 14:54:30 +0100

    Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    /* screens: {
+      // => @media (min-width: 640px) { ... }
+      // => @media (min-width: 768px) { ... }
+      // => @media (min-width: 1024px) { ... }
+      // => @media (min-width: 1280px) { ... }
+      // => @media (min-width: 1536px) { ... }
+    }*/
+  },
+  plugins: [],
+};
